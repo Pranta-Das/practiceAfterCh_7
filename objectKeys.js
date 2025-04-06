e0a978a3d7e94d89ac80b2deb455065a64943285
Pranta-Das <prantodas423@gmail.com>
2025-04-06 15:04:14 +0600

    check property inside the object

--- objectKeys.js
+++ objectKeys.js
@@ -10,3 +10,18 @@ console.log(keys);
 const values = Object.values(shop);
 console.log(values[1]);
 console.log(typeof(values[1]));
+
+// Checking any property is available inside the object or not.
+
+const fruit = {
+    mango:'From Bangladesh',
+    apple:{
+        formBangladesh:'there is no apple forest',
+        fromIndia:"Yes, kashmir is the biggest apple producer"
+    },
+    banana:'5000 dozon'
+}
+
+const fruitsKey= Object.keys(fruit);
+const hasMango = fruitsKey.includes('mango');
+console.log(hasMango);
