265a94914b06860a2cad2fefd929237b685361ca
Pranta-Das <prantodas423@gmail.com>
2025-04-06 14:52:34 +0600

    Object.keys() method

---
+++ objectKeys.js
@@ -0,0 +1,8 @@
+const shop = {
+    name:'pranto engineering workshop',
+    work:'making working material for house',
+    address:'sadaha, satkania, chattogram'
+};
+
+const keys = Object.keys(shop);
+console.log(keys);
